723ab348094bebdc823c1262fe807a495261c4c9
Pulkit Bhuwalka <pulkit.bosco05@gmail.com>
2014-02-26 02:36:12 -0500

    running with the server using weka-android.

--- src/io/pulkit/ubicomp/AMLTestApp/MainActivity.java
+++ src/io/pulkit/ubicomp/AMLTestApp/MainActivity.java
@@ -3,38 +3,31 @@ package io.pulkit.ubicomp.AMLTestApp;
 import android.app.Activity;
 import android.os.Bundle;
 
-import org.apache.http.HttpHost;
-import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.StringBufferInputStream;
 
-import smile.Network;
-import weka.classifiers.Classifier;
-import weka.classifiers.bayes.NaiveBayes;
 import weka.classifiers.bayes.NaiveBayesUpdateable;
-import weka.core.Attribute;
-import weka.core.FastVector;
-import weka.core.Instance;
-import weka.core.Instances;
 
 public class MainActivity extends Activity
 {
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
+        super.onCreate(savedInstanceState);
+
         HttpClient httpClient = new DefaultHttpClient();
         String result;
         try {
             result = httpClient.execute(
-                    new HttpGet("http://128.237.221.65:9979/ml_server/model/get"), new BasicResponseHandler());
+                    new HttpGet("http://128.237.223.111:9979/ml_server/model/get"), new BasicResponseHandler());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -43,6 +36,7 @@ public class MainActivity extends Activity
         try {
             ois = new ObjectInputStream(new StringBufferInputStream(result));
             NaiveBayesUpdateable naiveBayesUpdateable = (NaiveBayesUpdateable)ois.readObject();
+            System.out.println("Naive Bayes Model Dump");
             System.out.println(naiveBayesUpdateable.toString());
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -50,4 +44,5 @@ public class MainActivity extends Activity
             throw new RuntimeException(e);
         }
     }
+
 }
